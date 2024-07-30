41f4d6ebd91d3dd2b9e20c8e762a7cbad65a2818
rahuljat1349 <rahuldudi1349@gmail.com>
2024-07-30 06:46:21 +0530

    added route to get and send messages

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,64 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { Message } from "@/model/User";
+
+export async function POST(req: Request) {
+  await dbConnect();
+
+  const { username, content } = await req.json();
+
+  try {
+    const user = await UserModel.findOne({ username });
+
+    if (!username) {
+      return Response.json(
+        {
+          success: false,
+          Message: "User not found!",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    if (!user?.isAcceptingMessages) {
+      return Response.json(
+        {
+          success: false,
+          Message: "User is not accepting messages!",
+        },
+        {
+          status: 403,
+        }
+      );
+    }
+
+    const newMessage = { content, createdAt: new Date() };
+    user.messages.push(newMessage as Message);
+
+    await user.save();
+
+    return Response.json(
+      {
+        success: true,
+        Message: "Message sent successfully",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.log("failed to send Message.");
+
+    return Response.json(
+      {
+        success: false,
+        Message: "failed to send Message.",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
